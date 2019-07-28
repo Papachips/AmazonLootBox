340c414ea85f568ec8b0b0322dba59f4003e682a
Papachips <38638070+Papachips@users.noreply.github.com>
2019-07-27 20:22:46 -0400

    Add files via upload

---
+++ AmazonTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Nager.AmazonProductAdvertising;
+using Nager.AmazonProductAdvertising.Model;
+using System.Collections.Generic;
+
+namespace AmazonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Authentication
+            var authentication = new AmazonAuthentication();
+            authentication.AccessKey = "amazon access key here";
+            authentication.SecretKey = "amazon secret key here";
+
+            //Search critera and result
+            var wrapper = new AmazonWrapper(authentication, AmazonEndpoint.US, "amazon associate tag here");
+            var searchOperation = wrapper.ItemSearchOperation("canon eos", AmazonSearchIndex.Electronics);
+            searchOperation.MaxPrice(200000); //2000 USD
+            var xmlResponse = wrapper.Request(searchOperation);
+            var result = XmlHelper.ParseXml<ItemSearchResponse>(xmlResponse.Content);
+
+            //Create cart and add items to it
+            var items = new List<AmazonCartItem>();
+            items.Add(new AmazonCartItem("B00MH4QM1S"));
+            items.Add(new AmazonCartItem("B01EUHFAC6"));
+            var cart = wrapper.CartCreate(items);
+
+            //Debuging
+            /*wrapper.XmlReceived += (xml) => { System.Diagnostics.Debug.WriteLine(xml); };
+            wrapper.ErrorReceived += (errorResonse) => { System.Diagnostics.Debug.WriteLine(errorResonse.Error.Message); };
+            var errors = wrapper.Lookup(new string[] { "B00OQVZDJM", "B00ZV9RDKK" });*/
+        }
+    }
+}
